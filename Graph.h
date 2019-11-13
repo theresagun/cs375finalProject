6160c42a11efb278e4dc200127c5fcf15d52df44
David Walters <dwalter2@binghamton.edu>
2019-11-12 19:41:25 -0500

    parsing working

--- Graph.h
+++ Graph.h
@@ -1,1 +1,27 @@
+#ifndef GRAPH_H
+#define GRAPH_H
+#include <vector>
+#include <cstdlib>
+#include <string>
+#include <queue>
+class Node{
+public:
+  int id;
+  std::vector<int> adjacents;
+  std::string color;
+  int parent;
+  int d;
+  int f;
+  Node(int num){id=num;}
+};
 
+class Graph{
+public:
+  std::vector<Node> nodes;
+  std::queue<Node> q;
+  void DFS();
+  void DFSvisit(Node);
+  void BFS();
+};
+
+#endif
