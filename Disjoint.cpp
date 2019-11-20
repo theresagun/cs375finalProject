6bad043bfb49628d0adf7d1c34bf9265da5520cb
theresagun <33259276+theresagun@users.noreply.github.com>
2019-11-19 19:57:31 -0500

    frick

---
+++ Disjoint.cpp
@@ -0,0 +1,48 @@
+#include "Disjoint.h"
+#include <cstdlib>
+#include <vector>
+#include <iostream>
+
+using namespace std;
+
+void disjointSet::makeSet(Node n){
+  //make new LL, add to set
+  S.push_back(disjointLL(n));
+}
+
+Node disjointSet::findSet(Node n){
+  //returns representative, which is first element in disjointLL
+  return *(S[n.setObj].head);
+}
+
+void disjointSet::unionSets(Node x, Node y){
+  //adds set y to end of set x, deletes set y
+  Node xSet = findSet(x);
+  Node ySet = findSet(y);
+  int xIndex = 0;
+  int yIndex = 0;
+  for(int i = 0; i < S.size(); i++){
+    //if head is this we found it
+    if(S[i].head->id == xSet.id) xIndex = i;
+    if(S[i].head->id == ySet.id) yIndex = i;
+  }
+  //put the rest of y onto x
+  while(S[yIndex].head != nullptr){
+    S[xIndex].insert(S[yIndex].head->id);
+    S[yIndex].head = S[yIndex].head->next;
+  }
+  //delete y
+  S.erase(S.begin() + yIndex);
+}
+
+void disjointLL::insert(Node n){
+//  disjointNode newN = disjointNode(n.id);
+  //insert at end
+  tail->next = &n;
+  //update tail
+  tail = &n;
+}
+
+int main(int argc, char ** argv){
+  cout << "running" << endl;
+}
---
+++ Disjoint.h
@@ -0,0 +1,40 @@
+#ifndef DISJOINT_TG
+#define DISJOINT_TG
+#include <cstdlib>
+#include <vector>
+#include "Graph.h"
+
+// class disjointNode{
+//   //linked list node
+// public:
+//   int setMember; //data
+//   disjointNode * next; //next node
+//   disjointSet * setObj; //set object
+//   disjointNode(int n){
+//     setMember = n;
+//     next = nullptr;
+//     setObj = nullptr;
+//   }
+// };
+
+class disjointLL{
+  //linked list implementation
+public:
+  Node * head;
+  Node * tail;
+  disjointLL(Node n){
+    //disjointNode dn = disjointNode(n.id);
+    head = tail = &n;
+  }
+  void insert(Node);
+};
+
+class disjointSet{
+public:
+  std::vector<disjointLL> S;
+  void makeSet(Node);
+  Node findSet(Node);
+  void unionSets(Node, Node);
+};
+
+#endif
