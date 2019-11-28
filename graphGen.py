8725b38018bf93b0e065041b9766cda97e9e4ceb
Theresa Gundel <tgundel1@binghamton.edu>
2019-11-28 16:00:33 -0500

    fixed graph generator so every node is an adj of at least one other node

--- graphGen.py
+++ graphGen.py
@@ -8,19 +8,28 @@ def numGen(low, high, f):
         f.write("\n")
         #random adjs
         added = []
-        for i in range(5):
+        for j in range(5):
             toAdd = random.randint(low, high)
             if toAdd not in added:
                 added.append(toAdd)
                 f.write(str(toAdd))
                 f.write("\n")
+        #make sure all nodes are an adjaceny, so add every node as adjaceny of previous node
+        toAdd = i + 1
+        #if end of range, add first node in range
+        if toAdd == high + 1:
+            toAdd = low
+        #if it has not been added yet
+        if toAdd not in added:
+            f.write(str(toAdd))
+            f.write("\n")
         #-1 to indicate next node
         f.write(str(-1))
         f.write("\n")
 
 def main():
     numNodes = int(input("Enter desired number of nodes: "))
-    f = open("large2.txt", "w")
+    f = open("large1.txt", "w")
     for i in range(0, numNodes, numNodes//10):
         numGen(i, i+(numNodes//10)-1, f)
     f.close()
